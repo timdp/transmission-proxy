a3517eea1cf9dd60b1ebc920b5de39631d5bf003
Tim De Pauw <tim@pwnt.be>
2014-06-21 00:49:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+transmission-proxy
+==================
+
+A simple proxy server for the Transmission RPC API.
+
+The proxy only allows the `torrent-add` method. This makes it ideal for use with
+external services that are only allowed to push new torrents to your daemon.
+One example is [showRSS](http://showrss.info/).
+
+The app comes ready for deployment on [Heroku](http://heroku.com/), but being a
+standard [node.js](http://nodejs.org/) app, it can be run pretty much anywhere.
+
+See `config.json.example` for configuration information. 
+
+Author
+------
+
+[Tim De Pauw](http://tmdpw.eu/)
+
+License
+-------
+
+Copyright &copy; 2014 Tim De Pauw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ index.js
@@ -0,0 +1,84 @@
+'use strict';
+
+if (process.env.NEW_RELIC_LICENSE_KEY) {
+  require('newrelic');
+}
+
+var Transmission = require('transmission');
+var express = require('express');
+var logfmt = require('logfmt');
+var auth = require('http-auth');
+var randomstring = require('randomstring');
+
+var config = require('./config.json');
+
+var transmission = new Transmission(config.transmission);
+
+var processAddTorrent = function(req, res) {
+  var reply = function(err) {
+    res.json({
+      success: (err === null)
+    });
+  }
+  if (typeof req.body.arguments === 'object' &&
+      typeof req.body.arguments.filename === 'string') {
+    transmission.addUrl(req.body.arguments.filename, {
+      paused: config.add_paused
+    }, config.ignore_result ? function() {} : reply);
+  } else if (!config.ignore_result) {
+    reply('Invalid argument');
+  }
+  if (config.ignore_result) {
+    reply(null);
+  }
+}
+
+var assignSessionID = function(req, res) {
+  var sessionID = randomstring.generate(48);
+  res.set('X-Transmission-Session-Id', sessionID);
+  res.set('Content-Type', 'text/html; charset=ISO-8859-1');
+  res.send(new Buffer('<h1>409: Conflict</h1>' +
+    '<p><code>X-Transmission-Session-Id: ' + sessionID + '</code></p>'));
+};
+
+var authenticate = auth.connect(auth.basic({
+  realm: 'Transmission'
+}, function(username, password, callback) {
+  callback(username === config.username && password == config.password);
+}));
+
+var app = express();
+app.use(logfmt.requestLogger());
+app.use(function(req, res, next) {
+  req.rawBody = '';
+  req.on('data', function(data) {
+    req.rawBody += data;
+  })
+  req.on('end', function() {
+    try {
+      req.body = JSON.parse(req.rawBody);
+    } catch (e) {
+      req.body = {};
+    } finally {
+      next();
+    }
+  })
+});
+app.all(config.transmission.url, authenticate, function(req, res) {
+  res.set('Server', 'Transmission');
+  var sessionID = req.get('X-Transmission-Session-Id');
+  if (typeof sessionID === 'undefined') {
+    assignSessionID(req, res);
+  } else {
+    res.set('X-Transmission-Session-Id', sessionID);
+    if (typeof req.body === 'object' && req.body.method === 'torrent-add') {
+      processAddTorrent(req, res);
+    } else {
+      res.json({
+        success: false
+      });
+    }
+  }
+});
+
+app.listen(process.env.PORT || 8080);
