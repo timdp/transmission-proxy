72a3e8fc7eaeab68d3a3b38f18c7edd9afafe2b0
Tim De Pauw <hi@tmdpw.eu>
2015-05-05 22:24:53 +0200

    Adopting JS Standard Style

--- index.js
+++ index.js
@@ -1,252 +1,252 @@
-'use strict';
+'use strict'
 
 if (process.env.NEW_RELIC_LICENSE_KEY) {
-  require('newrelic');
+  require('newrelic')
 }
 
-var Transmission = require('transmission');
-var Q = require('q');
-var express = require('express');
-var logfmt = require('logfmt');
-var auth = require('http-auth');
-var pg = require('pg');
-var randomstring = require('randomstring');
+var Transmission = require('transmission')
+var Q = require('q')
+var express = require('express')
+var logfmt = require('logfmt')
+var auth = require('http-auth')
+var pg = require('pg')
+var randomstring = require('randomstring')
 
-var config = require('./config.json');
+var config = require('./config.json')
 
-var startTime = new Date();
-var lastAdd = null;
-var lastError = null;
-var queueProcessingStarted = null;
-var queueProcessingTime = -1;
-var retryTimeout = null;
+var startTime = new Date()
+var lastAdd = null
+var lastError = null
+var queueProcessingStarted = null
+var queueProcessingTime = -1
+var retryTimeout = null
 
-var transmission = new Transmission(config.transmission);
-var boundAddUrl = Q.nbind(transmission.addUrl, transmission);
+var transmission = new Transmission(config.transmission)
+var boundAddUrl = Q.nbind(transmission.addUrl, transmission)
 
-var logError = function(err) {
+var logError = function (err) {
   lastError = {
     time: new Date(),
     error: err
-  };
-  logfmt.error(err);
-};
+  }
+  logfmt.error(err)
+}
 
-var connectToDatabase = function() {
-  var url = process.env.DATABASE_URL || config.database_url;
+var connectToDatabase = function () {
+  var url = process.env.DATABASE_URL || config.database_url
   logfmt.log({
     action: 'connect',
     database_url: url
-  });
-  return Q.ninvoke(pg, 'connect', url);
-};
+  })
+  return Q.ninvoke(pg, 'connect', url)
+}
 
-var query = function(sql, params) {
+var query = function (sql, params) {
   logfmt.log({
     action: 'query',
     query: sql,
     params: params
-  });
-  var onDone = null;
+  })
+  var onDone = null
   return connectToDatabase()
-    .spread(function(client, done) {
-      onDone = done;
-      var args = [sql];
+    .spread(function (client, done) {
+      onDone = done
+      var args = [sql]
       if (params && params.length) {
-        args.push(params);
+        args.push(params)
       }
-      return Q.npost(client, 'query', args);
+      return Q.npost(client, 'query', args)
     })
-    .then(function(res) {
-      onDone();
-      return res.rows;
-    });
-};
+    .then(function (res) {
+      onDone()
+      return res.rows
+    })
+}
 
-var retrieveQueue = function() {
-  var sql = 'SELECT filename FROM queue';
+var retrieveQueue = function () {
+  var sql = 'SELECT filename FROM queue'
   return query(sql)
-    .then(function(rows) {
-      return rows.map(function(row) {
-        return row.filename;
-      });
-    });
-};
+    .then(function (rows) {
+      return rows.map(function (row) {
+        return row.filename
+      })
+    })
+}
 
-var addToQueue = function(filename) {
-  var sql = 'INSERT INTO queue (filename) VALUES ($1)';
-  return query(sql, [filename]);
-};
+var addToQueue = function (filename) {
+  var sql = 'INSERT INTO queue (filename) VALUES ($1)'
+  return query(sql, [filename])
+}
 
-var removeFromQueue = function(filenames) {
+var removeFromQueue = function (filenames) {
   if (!filenames.length) {
-    return;
+    return
   }
-  var ph = filenames.map(function(filename, idx) {
-    return '$' + (idx + 1);
-  }).join(',');
-  var sql = 'DELETE FROM queue WHERE filename IN (' + ph + ')';
-  return query(sql, filenames);
-};
+  var ph = filenames.map(function (filename, idx) {
+    return '$' + (idx + 1)
+  }).join(',')
+  var sql = 'DELETE FROM queue WHERE filename IN (' + ph + ')'
+  return query(sql, filenames)
+}
 
-var addToTransmission = function(filename) {
+var addToTransmission = function (filename) {
   logfmt.log({
     action: 'add',
     filename: filename
-  });
+  })
   return boundAddUrl(filename, {
     paused: config.add_paused
-  });
-};
+  })
+}
 
-var getAddToTransmissionPromise = function(filename) {
-  return function() {
+var getAddToTransmissionPromise = function (filename) {
+  return function () {
     return addToTransmission(filename)
-      .fail(function(err) {
-        throw new Error('Failed to add "' + filename + '": ' + err);
-      });
-  };
-};
+      .fail(function (err) {
+        throw new Error('Failed to add "' + filename + '": ' + err)
+      })
+  }
+}
 
-var processQueue = function() {
+var processQueue = function () {
   if (retryTimeout !== null) {
-    clearTimeout(retryTimeout);
-    retryTimeout = null;
+    clearTimeout(retryTimeout)
+    retryTimeout = null
   }
   if (queueProcessingStarted !== null) {
-    return;
+    return
   }
-  queueProcessingStarted = new Date();
-  var succeeded = [];
+  queueProcessingStarted = new Date()
+  var succeeded = []
   retrieveQueue()
-  .then(function(queue) {
-    return queue.reduce(function(promise, filename) {
-      return promise
-        .then(getAddToTransmissionPromise(filename))
-        .then(function() {
-          logfmt.log({
-            result: 'added',
-            filename: filename
-          });
-          succeeded.push(filename);
-        });
-    }, Q());
-  })
-  .fail(logError)
-  .then(function() {
-    return removeFromQueue(succeeded);
-  })
-  .fail(logError)
-  .fin(function() {
-    var endTime = new Date();
-    queueProcessingTime = endTime - queueProcessingStarted;
-    var time = (config.retry_after || 5 * 60) * 1000;
-    retryTimeout = setTimeout(processQueue, time);
-    queueProcessingStarted = null;
-  });
-};
+    .then(function (queue) {
+      return queue.reduce(function (promise, filename) {
+        return promise
+          .then(getAddToTransmissionPromise(filename))
+          .then(function () {
+            logfmt.log({
+              result: 'added',
+              filename: filename
+            })
+            succeeded.push(filename)
+          })
+      }, Q())
+    })
+    .fail(logError)
+    .then(function () {
+      return removeFromQueue(succeeded)
+    })
+    .fail(logError)
+    .fin(function () {
+      var endTime = new Date()
+      queueProcessingTime = endTime - queueProcessingStarted
+      var time = (config.retry_after || 5 * 60) * 1000
+      retryTimeout = setTimeout(processQueue, time)
+      queueProcessingStarted = null
+    })
+}
 
-var processAddTorrent = function(body) {
+var processAddTorrent = function (body) {
   if (typeof body.arguments === 'object' &&
-      typeof body.arguments.filename === 'string') {
-    var filename = body.arguments.filename;
+    typeof body.arguments.filename === 'string') {
+    var filename = body.arguments.filename
     lastAdd = {
       time: new Date(),
       filename: filename
-    };
+    }
     logfmt.log({
       action: 'enqueue',
       filename: filename
-    });
+    })
     addToQueue(filename)
       .fail(logError)
-      .fin(processQueue);
+      .fin(processQueue)
   }
-};
+}
 
-var assignSessionID = function(res) {
-  var sessionID = randomstring.generate(48);
-  res.set('X-Transmission-Session-Id', sessionID);
-  res.set('Content-Type', 'text/html; charset=ISO-8859-1');
+var assignSessionID = function (res) {
+  var sessionID = randomstring.generate(48)
+  res.set('X-Transmission-Session-Id', sessionID)
+  res.set('Content-Type', 'text/html; charset=ISO-8859-1')
   res.send('<h1>409: Conflict</h1>' +
-    '<p><code>X-Transmission-Session-Id: ' + sessionID + '</code></p>');
-};
+    '<p><code>X-Transmission-Session-Id: ' + sessionID + '</code></p>')
+}
 
 var authenticate = auth.connect(auth.basic({
-    realm: 'Transmission'
-  },
-  function(username, password, callback) {
-    callback(username === config.username && password === config.password);
-  }));
+  realm: 'Transmission'
+},
+  function (username, password, callback) {
+    callback(username === config.username && password === config.password)
+  }))
 
-var app = express();
+var app = express()
 
-app.use(logfmt.requestLogger());
+app.use(logfmt.requestLogger())
 
-app.use(function(req, res, next) {
-  req.rawBody = '';
-  req.on('data', function(data) {
-    req.rawBody += data;
-  });
-  req.on('end', function() {
+app.use(function (req, res, next) {
+  req.rawBody = ''
+  req.on('data', function (data) {
+    req.rawBody += data
+  })
+  req.on('end', function () {
     if (req.rawBody.length === 0) {
-      req.body = {};
-      next();
+      req.body = {}
+      next()
     } else {
       try {
-        req.body = JSON.parse(req.rawBody);
+        req.body = JSON.parse(req.rawBody)
       } catch (e) {
-        req.body = {};
+        req.body = {}
       } finally {
-        next();
+        next()
       }
     }
-  });
-});
-
-app.all('/', function(req, res) {
-  res.send('It works!');
-});
-
-app.get('/status', authenticate, function(req, res, next) {
-  retrieveQueue()
-  .then(function(queue) {
-      var lines = [];
-      lines.push('Started: ' + startTime);
-      lines.push('Last processing time: ' + (queueProcessingTime < 0 ? 'none' :
-        (queueProcessingTime / 1000).toFixed(2) + ' seconds'));
-      lines.push('Last add: ' + (lastAdd === null ? 'none' :
-        '[' + lastAdd.time + '] ' + lastAdd.filename));
-      lines.push('Last error: ' + (lastError === null ? 'none' :
-        '[' + lastError.time + '] ' + lastError.error));
-      lines.push('Pending torrents: ' + queue.length);
-      queue.forEach(function(filename) {
-        lines.push('- ' + filename);
-      });
-      res.set('Content-Type', 'text/plain');
-      res.send(lines.join('\n'));
   })
-  .fail(next);
-});
+})
 
-app.all(config.transmission.url, authenticate, function(req, res) {
-  res.set('Server', 'Transmission');
-  var sessionID = req.get('X-Transmission-Session-Id');
+app.all('/', function (req, res) {
+  res.send('It works!')
+})
+
+app.get('/status', authenticate, function (req, res, next) {
+  retrieveQueue()
+    .then(function (queue) {
+      var lines = []
+      lines.push('Started: ' + startTime)
+      lines.push('Last processing time: ' + (queueProcessingTime < 0 ? 'none' :
+          (queueProcessingTime / 1000).toFixed(2) + ' seconds'))
+      lines.push('Last add: ' + (lastAdd === null ? 'none' :
+          '[' + lastAdd.time + '] ' + lastAdd.filename))
+      lines.push('Last error: ' + (lastError === null ? 'none' :
+          '[' + lastError.time + '] ' + lastError.error))
+      lines.push('Pending torrents: ' + queue.length)
+      queue.forEach(function (filename) {
+        lines.push('- ' + filename)
+      })
+      res.set('Content-Type', 'text/plain')
+      res.send(lines.join('\n'))
+    })
+    .fail(next)
+})
+
+app.all(config.transmission.url, authenticate, function (req, res) {
+  res.set('Server', 'Transmission')
+  var sessionID = req.get('X-Transmission-Session-Id')
   if (typeof sessionID === 'undefined') {
-    assignSessionID(res);
+    assignSessionID(res)
   } else {
-    res.set('X-Transmission-Session-Id', sessionID);
+    res.set('X-Transmission-Session-Id', sessionID)
     if (typeof req.body === 'object' && req.body.method === 'torrent-add') {
-      processAddTorrent(req.body);
+      processAddTorrent(req.body)
       res.json({
         success: true
-      });
+      })
     } else {
       res.json({
         success: false
-      });
+      })
     }
   }
-});
+})
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080)
